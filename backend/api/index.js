a2fe1a83edab7d11a8f3cf0a731810d6e1a2ba90
seongyeon123-s <tjddus1257@naver.com>
2025-10-03 14:52:03 +0900

    feat: add offline shelter save and news

--- backend/api/index.js
+++
@@ -1,6 +0,0 @@
-// backend/api/index.js
-const serverless = require("serverless-http");
-const { app } = require("../index.js"); // backend/index.js에서 export한 app 가져오기
-
-// Vercel은 default export된 함수(handler)를 실행함
-module.exports = serverless(app);
